ca7295bb8410d2d4c5599584cfd3d1e7c47fc1b0
Simon Daley-Yates <sdaley@star.sr.bham.ac.uk>
2017-11-27 14:51:21 +0000

    changed some names for clarity.

---
+++ old_spherical/definitions.h
@@ -0,0 +1,67 @@
+#define  PHYSICS                 HD
+#define  DIMENSIONS              2
+#define  COMPONENTS              3
+#define  GEOMETRY                SPHERICAL
+#define  BODY_FORCE              VECTOR
+#define  COOLING                 NO
+#define  RECONSTRUCTION          PARABOLIC
+#define  TIME_STEPPING           RK3
+#define  DIMENSIONAL_SPLITTING   NO
+#define  NTRACER                 0
+#define  USER_DEF_PARAMETERS     13
+
+/* -- physics dependent declarations -- */
+
+#define  EOS                     IDEAL
+#define  ENTROPY_SWITCH          NO
+#define  THERMAL_CONDUCTION      NO
+#define  VISCOSITY               NO
+#define  ROTATING_FRAME          YES
+
+/* -- user-defined parameters (labels) -- */
+
+#define  Eta                     0
+#define  R_RATIO                 1
+#define  Cs_P                    2
+#define  M_RATIO                 3
+#define  L_RATIO                 4
+#define  TT                      5
+#define  MU                      6
+#define  AA                      7
+#define  Bb                      8
+#define  QQ                      9
+#define  aa_eff                  10
+#define  BB                      11
+#define  OMEGA                   12
+
+/* [Beg] user-defined constants (do not change this line) */
+
+#define  UNIT_DENSITY            1.0e-12
+#define  UNIT_LENGTH             6.955e+10*9.0
+#define  UNIT_VELOCITY           1.0e+5
+#define  UNIT_MASS               (UNIT_DENSITY*pow(UNIT_LENGTH,3))
+#define  UNIT_TIME               (UNIT_LENGTH/UNIT_VELOCITY)
+#define  UNIT_G                  (CONST_G*UNIT_DENSITY*pow(UNIT_TIME,2))
+#define  UNIT_kB                 ((CONST_kB*pow(UNIT_TIME,2))/(UNIT_DENSITY*pow(UNIT_LENGTH,5)))
+#define  UNIT_B                  (sqrt((4.0*CONST_PI*UNIT_DENSITY*UNIT_VELOCITY*UNIT_VELOCITY)))
+#define  UNIT_L                  (pow(UNIT_TIME,-3)*(UNIT_DENSITY*pow(UNIT_LENGTH,5)))
+#define  tyear                   3.15569e+7
+#define  tday                    8.64e+4
+#define  L_sun                   3.846e+33
+#define  VTK_VECTOR_DUMP         YES
+#define  GLM_EXTENDED            NO
+#define  CAK                     YES
+#define  CHOMBO_LOGR             YES
+#define  CHOMBO_CONS_AM          NO
+
+/* [End] user-defined constants (do not change this line) */
+
+/* -- supplementary constants (user editable) -- */ 
+
+#define  INITIAL_SMOOTHING   NO
+#define  WARNING_MESSAGES    NO
+#define  PRINT_TO_FILE       NO
+#define  INTERNAL_BOUNDARY   YES
+#define  SHOCK_FLATTENING    NO
+#define  CHAR_LIMITING       NO
+#define  LIMITER             VANLEER_LIM
